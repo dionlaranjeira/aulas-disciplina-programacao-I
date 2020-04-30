832037ea468c7deced6fe0ce158c8c55d633019b
Dion Laranjeira <dionribeiro.rr@gmail.com>
2020-04-30 16:42:16 -0400

    Commit Inicial
    Aula de Interfaces adicionada

---
+++ Aula-interfaces/src/aula/interfaces/AulaInterfaces.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula.interfaces;
+
+/**
+ *
+ * @author dion_
+ */
+public class AulaInterfaces {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Aula dia 30/04/2020");
+        System.out.println("Interfaces");
+    }
+    
+}
